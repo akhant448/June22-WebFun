875f0c2875425246494728ef32dcba160ebe7018
akhant448 <frank042001@outlook.com>
2022-06-23 23:11:08 -0700

    updated

---
+++ assignments/week3/likes/part 1/script.js
@@ -0,0 +1,8 @@
+var x = 3
+
+function increaseLike(element) {
+    console.log ("Button is clicked")
+    x++
+    document.getElementById('num1').innerHTML = x
+}
+
---
+++ assignments/week3/likes/part 2/script.js
@@ -0,0 +1,23 @@
+var x = 9
+
+function increaseLike1(element) {
+    console.log ("Button is clicked")
+    x++
+    document.getElementById('num1').innerHTML = x
+}
+
+var i = 12
+
+function increaseLike2(element) {
+    console.log ("Button is clicked")
+    i++
+    document.getElementById('num2').innerHTML = i
+}
+
+var y = 9
+
+function increaseLike3(element) {
+    console.log ("Button is clicked")
+    y++
+    document.getElementById('num3').innerHTML = y
+}
